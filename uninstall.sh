d853daba4e0623ddf161e48ee2453d56ad9cf6bc
Corona <wswzgdg@gmail.com>
2026-07-03 03:45:25 +0800

    重构 zram 初始化链并整理 WebUI 行为

--- uninstall.sh
+++ uninstall.sh
@@ -2,13 +2,10 @@
 MODPATH=${MODPATH:-${0%/*}}
 CONFIG_DIR="$MODPATH/config"
 RUNTIME_CONF="$CONFIG_DIR/runtime.conf"
-
 get_conf_value() { [ -f "$1" ] && grep -m1 "^$2=" "$1" | cut -d'=' -f2-; }
-
 swapfile=$(get_conf_value "$CONFIG_DIR/swap.conf" "path")
 [ -z "$swapfile" ] && swapfile=$(get_conf_value "$RUNTIME_CONF" "swapfile_path")
 [ -z "$swapfile" ] && swapfile="$MODPATH/swapfile.img"
-
 rm -rf "$CONFIG_DIR"
 rm -rf "$MODPATH/scripts.d/.logs"
 swapoff "$swapfile" 2>/dev/null
